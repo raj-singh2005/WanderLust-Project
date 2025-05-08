5aea29cbc7dc4c3b10867469bdc479b14c9c3d40
raj.singh2005 <raj.singh2005.dev@gmail.com>
2025-05-09 00:37:18 +0530

    fixing port issue

--- app.js
+++ app.js
@@ -116,10 +116,10 @@ app.use((err,req,res,next)=>{
 })
 
 //creating server
-app.listen(8080,()=>{
-    console.log("server is listening on port : 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
 });
-
 
 
 
